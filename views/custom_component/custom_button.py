5ec574df439169ad8c4e65b1333d0323656ac2f5
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2023-11-25 11:46:24 +0200

    implement a custom button

---
+++ views/custom_component/custom_button.py
@@ -0,0 +1,32 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QPainter, QBrush, QColor, QFont
+from PyQt5.QtWidgets import QPushButton
+
+
+class RoundButton(QPushButton):
+    def __init__(self, name):
+        super().__init__()
+
+        self.normal_color = QColor("#4682A9")
+        self.hover_color = QColor("#91C8E4")
+
+        palette = QPalette()
+        palette.setColor(QPalette.ButtonText, Qt.white)
+        self.setPalette(palette)
+        self.setText(name)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        if self.underMouse():
+            painter.setBrush(QBrush(self.hover_color))
+        else:
+            painter.setBrush(QBrush(self.normal_color))
+
+        painter.drawRoundedRect(self.rect(), 10, 10)
+
+        painter.setPen(Qt.white)
+        painter.setFont(QFont("Arial", 12, QFont.Bold))
+        text_rect = self.rect()
+        painter.drawText(text_rect, Qt.AlignCenter, self.text())
