2484d85c71fed5df02fddbd135b078fdbeb18bc2
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-11-27 15:01:13 +0200

    fix[frontend]: change left side widget name

--- views/sniffing/vhdl_editor_buttons.py
+++ views/sniffing/vhdl_editor_buttons.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 
 class VhdlEditorButtons(QWidget):
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self._load_button = QPushButton('Load')
