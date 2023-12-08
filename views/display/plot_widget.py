9cfd51a58c269bcab6ac4ada4f343430283a60ac
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2023-12-08 14:58:54 +0200

    feat[frontend]: implement plot appearance

--- views/display/plot_widget.py
+++ views/display/plot_widget.py
@@ -8,6 +8,7 @@ class PlotWidget(QWidget):
 
         self.fixed_label = QLabel(f'D{channel_name}')
         self.channel_label = QLabel(f'Channel {channel_name}')
+        self.channel_name = channel_name
         self.plot_widget = pg.PlotWidget()
 
         self.set_size()
@@ -31,9 +32,30 @@ class PlotWidget(QWidget):
 
         self.setLayout(self.layout)
 
+        self.plot_appearance()
+
+    def plot_appearance(self):
+        self.plot_widget.setBackground((255, 255, 255))
+
+        self.plot_widget.setTitle(f'Channel {self.channel_name} Data', color='k')
+
+        self.plot_widget.setLabel('bottom', 'Time', color='k')
+        self.plot_widget.setLabel('left', 'Amplitude', color='k')
+
+        pen = pg.mkPen(color=(0, 120, 255), width=2)
+        self.plot_widget.plot(pen=pen, symbol='o', symbolSize=5)
+
+        self.plot_widget.getAxis("bottom").setTextPen((0, 0, 0))
+        self.plot_widget.getAxis("left").setTextPen((0, 0, 0))
+
+        self.plot_widget.showGrid(x=True, y=True, alpha=0.7)
+        self.plot_widget.addLegend()
+
+        self.plot_widget.setMouseEnabled(x=True, y=True)
+
     def set_size(self):
         self.plot_widget.setMinimumWidth(700)
         self.plot_widget.setMaximumWidth(1300)
 
-        self.plot_widget.setMinimumHeight(130)
+        self.plot_widget.setMinimumHeight(200)
         self.plot_widget.setMaximumHeight(400)
