56e112b4cab34ec7e3ffd06e9b369999e8feeda0
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2023-12-09 15:47:32 +0200

    feat[frontend]: Implement MenuBar with add path and exit actions

---
+++ views/common/menubar.py
@@ -0,0 +1,36 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QApplication, QAction, QMenuBar, QFileDialog
+
+
+class MenuBar(QMenuBar):
+
+    add_new_path = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.init_ui()
+
+    def init_ui(self):
+        file_menu = self.addMenu('File')
+
+        add_new_path_action = QAction('Add New Path', self)
+        add_new_path_action.setShortcut('Ctrl+O')
+        add_new_path_action.triggered.connect(self.open_directory_dialog)
+        file_menu.addAction(add_new_path_action)
+
+        exit_action = QAction('Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.triggered.connect(self.close_application)
+        file_menu.addAction(exit_action)
+
+    def open_directory_dialog(self):
+        directory_path = QFileDialog.getExistingDirectory(self, 'Select Project Directory')
+        if directory_path:
+            self.emit_add_new_path(directory_path)
+
+    @staticmethod
+    def close_application():
+        QApplication.instance().quit()
+
+    def emit_add_new_path(self, project_path):
+        self.add_new_path.emit(project_path)
