15bd31277cfba297d59d4aaaedb020a75e685cbd
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-12-13 15:48:05 +0200

    fix[frontend]: refactor button names

--- views/sniffing/buttons/vhdl_editor_buttons.py
+++ views/sniffing/buttons/vhdl_editor_buttons.py
@@ -1,20 +1,23 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from controller.sniffing_controller.buttons_controller.vhdl_editor_buttons_controller import \
+    VhdlEditorButtonsController
 
 
 class VhdlEditorButtons(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self._load_button = QPushButton('Load')
-        self._save_button = QPushButton('Save')
-        self._save_as_button = QPushButton('Save As')
-        self._revert_button = QPushButton('Revert')
+
+        self.load_button = QPushButton('Load')
+        self.save_button = QPushButton('Save')
+        self.save_as_button = QPushButton('Save As')
+        self.revert_button = QPushButton('Revert')
 
         self.init_ui()
 
     def init_ui(self):
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(self._load_button)
-        self.layout().addWidget(self._save_button)
-        self.layout().addWidget(self._save_as_button)
-        self.layout().addWidget(self._revert_button)
+        self.layout().addWidget(self.load_button)
+        self.layout().addWidget(self.save_button)
+        self.layout().addWidget(self.save_as_button)
+        self.layout().addWidget(self.revert_button)
