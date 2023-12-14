174199e306dda12b20581e13cc2231a6c077051a
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2023-12-14 23:21:11 +0200

    fix[core]: handle top level file name

--- core/vhdl_generator.py
+++ core/vhdl_generator.py
@@ -2,8 +2,7 @@ import os
 
 from jinja2 import Environment, select_autoescape, PackageLoader
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_PATH = os.path.join(ROOT_DIR, 'models', 'VHDL')
+from controllers.project_path_controller import ProjectPathController
 
 
 class VhdlGenerator:
@@ -13,26 +12,17 @@ class VhdlGenerator:
             loader=PackageLoader("models", "vhdl_templates"),
             autoescape=select_autoescape()
         )
+        self.project_path_controller = ProjectPathController.get_instance()
 
     def render_template(self, template_name, configurations, output_path):
+        top_level_name = self.project_path_controller.get_top_level_name()
         template = self.env.get_template(template_name)
         rendered_content = template.render(configurations)
 
-        template_path = os.path.join(output_path, template_name.replace('.jinja', ''))
+        if template_name == 'top_level.vhd.jinja':
+            template_path = os.path.join(output_path, f"{top_level_name}.vhd")
+        else:
+            template_path = os.path.join(output_path, template_name.replace('.jinja', ''))
+
         with open(template_path, 'w') as file:
             file.write(rendered_content)
-
-
-# Dummy Data
-renderer = VhdlGenerator()
-
-config = {
-    'channels_number': 6,
-    'output_channels_number': 8,
-    'clocks_per_bit': 5208,
-    'option': "UART",
-    'project_name': "new_test",
-    'bits_per_frame': 8,
-}
-
-renderer.render_template('top_level.vhd.jinja', config, PROJECT_PATH)
