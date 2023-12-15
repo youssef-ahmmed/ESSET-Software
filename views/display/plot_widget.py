df4ee7ac31d21c429733f1dff906f4d99552815c
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2023-12-15 04:07:07 +0200

    feat[frontend]: used QTableWidget instead of QWidget

--- views/display/plot_widget.py
+++ views/display/plot_widget.py
@@ -1,45 +1,47 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QFormLayout
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QLabel
 import pyqtgraph as pg
+from PyQt5.QtCore import Qt
 
 
-class PlotWidget(QWidget):
-
-    def __init__(self, channel_name):
+class PlotWidget(QTableWidget):
+    def __init__(self, channel_number):
         super().__init__()
 
-        self.fixed_label = QLabel(f'D{channel_name}')
-        self.channel_label = QLabel(f'Channel {channel_name}')
-        self.channel_name = channel_name
-        self.plot_widget = pg.PlotWidget()
+        self.setColumnCount(2)
+        self.setRowCount(1)
 
-        self.set_size()
+        self.channel_number = channel_number
+        self.channel_label = None
+        self.plot_widget = None
+
+        self.row_size()
+        self.create_widgets()
         self.init_ui()
 
     def init_ui(self):
-        self.label_layout = QFormLayout()
+        self.setItem(0, 0, QTableWidgetItem())
+        self.setItem(0, 1, QTableWidgetItem())
 
-        v_spacing = 15
-        h_spacing = 1
+        self.item(0, 0).setTextAlignment(Qt.AlignCenter)
+        self.item(0, 0).setText(f'Channel {self.channel_number}')
+        self.setCellWidget(0, 1, self.plot_widget)
 
-        self.label_layout.setVerticalSpacing(v_spacing)
-        self.label_layout.setHorizontalSpacing(h_spacing)
-
-        self.label_layout.addWidget(self.fixed_label)
-        self.label_layout.addWidget(self.channel_label)
-
-        self.layout = QHBoxLayout()
-        self.layout.addLayout(self.label_layout)
-        self.layout.addWidget(self.plot_widget)
-
-        self.setLayout(self.layout)
+        self.horizontalHeader().setVisible(False)
+        self.verticalHeader().setVisible(False)
 
         self.plot_appearance()
 
+    def row_size(self):
+        self.setColumnWidth(1, 1150)
+        self.setRowHeight(0, 200)
+
+    def create_widgets(self):
+        self.channel_label = QLabel()
+        self.plot_widget = pg.PlotWidget()
+
     def plot_appearance(self):
         self.plot_widget.setBackground((255, 255, 255))
-
-        self.plot_widget.setTitle(f'Channel {self.channel_name} Data', color='k')
-
+        self.plot_widget.setTitle(f'Channel {self.channel_number} Data', color='k')
         self.plot_widget.setLabel('bottom', 'Time', color='k')
         self.plot_widget.setLabel('left', 'Amplitude', color='k')
 
@@ -51,12 +53,4 @@ class PlotWidget(QWidget):
 
         self.plot_widget.showGrid(x=True, y=True, alpha=0.7)
         self.plot_widget.addLegend()
-
         self.plot_widget.setMouseEnabled(x=True, y=True)
-
-    def set_size(self):
-        self.plot_widget.setMinimumWidth(700)
-        self.plot_widget.setMaximumWidth(1300)
-
-        self.plot_widget.setMinimumHeight(200)
-        self.plot_widget.setMaximumHeight(400)
--- views/display/waveform_widget.py
+++ views/display/waveform_widget.py
@@ -1,65 +1,82 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFormLayout, QScrollArea
+from PyQt5.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QScrollArea, QVBoxLayout
+
 from views.display.plot_widget import PlotWidget
 
 
 class WaveformWidget(QWidget):
-
     def __init__(self):
         super().__init__()
 
         self.channel_numbers = 8
+        self.table_widget = None
         self.plot_widgets = []
 
         self.init_ui()
 
     def init_ui(self):
-        row_spacing = 15
-        self.base_layout = QFormLayout()
-        self.base_layout.setVerticalSpacing(row_spacing)
+        self.table_widget = QTableWidget(self)
+        self.table_widget.setColumnCount(1)
+        self.table_widget.setRowCount(self.channel_numbers)
 
-        for channel in range(self.channel_numbers):
-            plot_widget = PlotWidget(channel)
-            self.plot_widgets.append(plot_widget)
+        self.table_size()
+        self.create_rows()
 
-        for plot_widget in self.plot_widgets:
-            self.base_layout.addWidget(plot_widget)
-
-        scroll_area = QScrollArea()
+        scroll_area = QScrollArea(self)
         scroll_area.setWidgetResizable(True)
-
-        scroll_widget = QWidget()
-        scroll_widget.setLayout(self.base_layout)
-
-        scroll_area.setWidget(scroll_widget)
+        scroll_area.setWidget(self.table_widget)
 
         main_layout = QVBoxLayout(self)
         main_layout.addWidget(scroll_area)
 
-        self.setLayout(main_layout)
+    def table_size(self):
+        self.table_widget.setMinimumWidth(700)
+        self.table_widget.setMinimumHeight(400)
+
+    def create_rows(self):
+        for channel in range(self.channel_numbers):
+            plot_widget = PlotWidget(channel)
+            self.table_widget.setCellWidget(channel, 0, plot_widget)
+            self.table_widget.setRowHeight(channel, 200)
+            self.table_widget.setColumnWidth(0, 1320)
+
+            vertical_header_item = QTableWidgetItem(f'D{channel}')
+            self.table_widget.setVerticalHeaderItem(channel, vertical_header_item)
+
+            self.plot_widgets.append(plot_widget)
+
+        self.table_widget.horizontalHeader().setVisible(False)
 
     def toggle_visibility(self, channel_number):
-        no_visible_plots = self.get_number_of_visible_channels()
-        if no_visible_plots == 1 and channel_number == 0:
+        visible_channels = [i for i, plot_widget in enumerate(self.plot_widgets) if plot_widget.isVisible()]
+        num_visible_channels = len(visible_channels)
+
+        if channel_number == 0 and num_visible_channels == 1:
             self.plot_widgets[0].show()
 
-        elif no_visible_plots == 1 and channel_number != 0:
+        elif channel_number != 0 and num_visible_channels == 1:
             self.plot_widgets[0].show()
             self.plot_widgets[channel_number].setVisible(not self.plot_widgets[channel_number].isVisible())
 
-        elif no_visible_plots > 1:
+        elif num_visible_channels > 0:
             self.plot_widgets[channel_number].setVisible(not self.plot_widgets[channel_number].isVisible())
+
+        self.update_row_heights(self.plot_widgets)
+
+    def update_row_heights(self, plot_widgets):
+        for i, plot_widget in enumerate(plot_widgets):
+            self.table_widget.setRowHeight(i, 200 if plot_widget.isVisible() else 0)
 
     def hide_all_channels(self):
-        for plot_number in range(0, len(self.plot_widgets)):
+        for plot_number, plot_widget in enumerate(self.plot_widgets):
             if plot_number == 0:
-                self.plot_widgets[plot_number].show()
+                plot_widget.setVisible(True)
             else:
-                self.plot_widgets[plot_number].hide()
+                plot_widget.setVisible(False)
+                self.table_widget.setRowHeight(plot_number, 0)
 
     def show_all_channels(self):
-        for plot_number in range(0, len(self.plot_widgets)):
-            self.plot_widgets[plot_number].show()
+        for plot_widget in self.plot_widgets:
+            plot_widget.setVisible(True)
 
-    def get_number_of_visible_channels(self):
-        return sum(1 for plot_widget in self.plot_widgets if plot_widget.isVisible())
-
+        for i, plot_widget in enumerate(self.plot_widgets):
+            self.table_widget.setRowHeight(i, 200)
