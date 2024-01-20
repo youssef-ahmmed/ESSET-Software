4b291ae4dc7b3efaf34aa1bdb6087cb01cfd22ab
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-20 16:37:00 +0200

    feat[backend]: implement channels data table dao & dto

---
+++ models/dto/channels_data_dto.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+
+class ChannelsDataDto:
+    def __init__(self, sniffed_data_id: int, channel_number: int, channel_data: Any):
+        self.sniffed_data_id = sniffed_data_id
+        self.channel_number = channel_number
+        self.channel_data = channel_data
