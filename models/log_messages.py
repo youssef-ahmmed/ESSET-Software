263fba33112421678428d0f8657924f5a21a6b85
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-01-20 17:40:29 +0200

    feat[frontend]: Add all log messages

---
+++ models/log_messages.py
@@ -0,0 +1,16 @@
+# Path Messages
+QUARTUS_PATH_SPECIFIED = "Quartus Path Specified Successfully."
+NO_QUARTUS_PATH = "No Quartus Path Specified."
+
+# UART Messages
+UART_RESET = "UART Configurations is Reset."
+UART_CONFIG_SET = "UART Configurations Set Successfully."
+
+# SPI Messages
+SPI_RESET = "SPI Configurations is Reset."
+SPI_CONFIG_SET = "SPI Configurations Set Successfully."
+
+# Synthesize Messages
+SYNTHESIZE_INITIATED = "Synthesizing Process Initiated. Please Await Completion..."
+SYNTHESIZE_SUCCESS = "Synthesizing Process Completed Successfully in:"
+SYNTHESIZE_FAILED = "Synthesizing Process Failed. Please review and try again."
