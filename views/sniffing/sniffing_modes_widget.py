1814e44e92cf4f5b923bcda2eaf12418e2a432d5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-20 23:19:42 +0200

    fix[frontend]: apply splitter layout for modes and hardware config widgets

--- views/sniffing/sniffing_modes_widget.py
+++ views/sniffing/sniffing_modes_widget.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QTabWidget, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QTabWidget, QHBoxLayout, QSplitter
 
 from views.sniffing.expert_mode_widget import ExpertModeWidget
 from views.sniffing.hardware_configuration_widget import HardwareConfigurations
@@ -11,6 +12,7 @@ class SniffingModesWidget(QWidget):
         super().__init__()
 
         self.modes_widget = QTabWidget(self)
+        self.hardware_config = HardwareConfigurations()
 
         self.init_ui()
 
@@ -18,10 +20,23 @@ class SniffingModesWidget(QWidget):
         self.setLayout(QHBoxLayout())
 
         self.layout().addWidget(self.modes_widget)
-        self.layout().addWidget(HardwareConfigurations())
+        self.layout().addWidget(self.hardware_config)
 
         simple_mode = SimpleModeWidget()
         expert_mode = ExpertModeWidget()
 
         self.modes_widget.addTab(simple_mode, "Simple Mode")
         self.modes_widget.addTab(expert_mode, "Expert Mode")
+
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(self.modes_widget)
+        splitter.addWidget(self.hardware_config)
+
+        total_size = self.modes_widget.sizeHint().width() + self.hardware_config.sizeHint().width()
+
+        size_vhdl = 0.99 * total_size
+        size_settings = 0.01 * total_size
+
+        splitter.setSizes([int(size_vhdl), int(size_settings)])
+
+        self.layout().addWidget(splitter)
