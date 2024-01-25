d5c298074a8359a63c37f1d5d0df6454e4a7c0f4
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2024-01-25 15:25:36 +0200

    refactor[frontend]: refactor spi settings function, reduce functions parameters

--- views/sniffing/communication_protocols/spi_config.py
+++ views/sniffing/communication_protocols/spi_config.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, \
 from loguru import logger
 
 from models import log_messages
-from views.sniffing.dialogs.channel_pins_dialog import ChannelPinsDialog
 
 
 class SpiConfigurations(QDialog):
@@ -18,6 +17,7 @@ class SpiConfigurations(QDialog):
         self.initial_values = ["Select Channel", "ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"]
 
         self.init_ui()
+        self.set_up()
 
     def init_ui(self):
         self.setWindowTitle("Spi Settings")
@@ -28,6 +28,7 @@ class SpiConfigurations(QDialog):
         y = int((screen_geometry.height() - self.height()) / 2)
         self.move(x, y)
 
+    def set_up(self):
         self.spi_settings()
         self.create_layout()
         self.connect_signals()
@@ -40,74 +41,46 @@ class SpiConfigurations(QDialog):
         row_spacing = 15
         self.base_layout.setVerticalSpacing(row_spacing)
 
-        self.mosi_label = QLabel("MOSI")
-        self.mosi_combo = QComboBox()
+        mosi_label, self.mosi_combo = self.create_channels_combo_box("MOSI", QComboBox())
+        miso_label, self.miso_combo = self.create_channels_combo_box("MISO", QComboBox())
+        clock_label, self.clock_combo = self.create_channels_combo_box("Clock", QComboBox())
+        enable_label, self.enable_combo = self.create_channels_combo_box("Enable", QComboBox())
 
-        self.mosi_combo.addItem("Select Channel")
-        self.mosi_combo.addItems(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"])
-        self.mosi_combo.setItemData(0, 0, role=Qt.UserRole - 1)
-        self.mosi_combo.setCurrentIndex(0)
+        significant_bit_label, self.significant_bit_combo = self.create_setup_combo_box("Significant Bit",
+                                                                                        ["L", "M"],
+                                                                                        "L")
+        bits_per_transfer_label, self.bits_per_transfer_combo = self.create_setup_combo_box("Bits Per Transfer",
+                                                                                            ["8", "7", "6", "5"],
+                                                                                            "8")
+        clock_state_label, self.clock_state_combo = self.create_setup_combo_box("Clock State",
+                                                                                ["0", "1"],
+                                                                                "0")
+        clock_phase_label, self.clock_phase_combo = self.create_setup_combo_box("Clock Phase",
+                                                                                ["0", "1"],
+                                                                                "0")
 
-        self.miso_label = QLabel("MISO")
-        self.miso_combo = QComboBox()
+        self.base_layout.addRow(mosi_label, self.mosi_combo)
+        self.base_layout.addRow(miso_label, self.miso_combo)
+        self.base_layout.addRow(clock_label, self.clock_combo)
+        self.base_layout.addRow(enable_label, self.enable_combo)
+        self.base_layout.addRow(significant_bit_label, self.significant_bit_combo)
+        self.base_layout.addRow(bits_per_transfer_label, self.bits_per_transfer_combo)
+        self.base_layout.addRow(clock_state_label, self.clock_state_combo)
+        self.base_layout.addRow(clock_phase_label, self.clock_phase_combo)
 
-        self.miso_combo.addItem("Select Channel")
-        self.miso_combo.addItems(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"])
-        self.miso_combo.setItemData(0, 0, role=Qt.UserRole - 1)
-        self.miso_combo.setCurrentIndex(0)
+    def create_channels_combo_box(self, label_text, combo_box):
+        label = QLabel(label_text)
+        combo_box.addItems(self.initial_values)
+        combo_box.setItemData(0, 0, role=Qt.UserRole - 1)
+        combo_box.setCurrentIndex(0)
+        return label, combo_box
 
-        self.clock_label = QLabel("Clock")
-        self.clock_combo = QComboBox()
-
-        self.clock_combo.addItem("Select Channel")
-        self.clock_combo.addItems(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"])
-        self.clock_combo.setItemData(0, 0, role=Qt.UserRole - 1)
-        self.clock_combo.setCurrentIndex(0)
-
-        self.enable_label = QLabel("Enable")
-        self.enable_combo = QComboBox()
-
-        self.enable_combo.addItem("Select Channel")
-        self.enable_combo.addItems(["ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"])
-        self.enable_combo.setItemData(0, 0, role=Qt.UserRole - 1)
-        self.enable_combo.setCurrentIndex(0)
-
-        self.significant_bit_label = QLabel("Significant Bit")
-        self.significant_bit_combo = QComboBox()
-        self.significant_bit_combo.addItems(["L", "M"])
-
-        default_significant_bit = 'L'
-        self.significant_bit_combo.setCurrentText(default_significant_bit)
-
-        self.bits_per_transfer_label = QLabel("Bits Per Transfer")
-        self.bits_per_transfer_combo = QComboBox()
-        self.bits_per_transfer_combo.addItems(["8", "7", "6", "5"])
-
-        default_bits_per_transfer = '8'
-        self.bits_per_transfer_combo.setCurrentText(default_bits_per_transfer)
-
-        self.clock_state_label = QLabel("Clock State")
-        self.clock_state_combo = QComboBox()
-        self.clock_state_combo.addItems(["0", "1"])
-
-        default_clock_state = '0'
-        self.clock_state_combo.setCurrentText(default_clock_state)
-
-        self.clock_phase_label = QLabel("Clock Phase")
-        self.clock_phase_combo = QComboBox()
-        self.clock_phase_combo.addItems(["0", "1"])
-
-        default_clock_phase = '0'
-        self.clock_phase_combo.setCurrentText(default_clock_phase)
-
-        self.base_layout.addRow(self.mosi_label, self.mosi_combo)
-        self.base_layout.addRow(self.miso_label, self.miso_combo)
-        self.base_layout.addRow(self.clock_label, self.clock_combo)
-        self.base_layout.addRow(self.enable_label, self.enable_combo)
-        self.base_layout.addRow(self.significant_bit_label, self.significant_bit_combo)
-        self.base_layout.addRow(self.bits_per_transfer_label, self.bits_per_transfer_combo)
-        self.base_layout.addRow(self.clock_state_label, self.clock_state_combo)
-        self.base_layout.addRow(self.clock_phase_label, self.clock_phase_combo)
+    def create_setup_combo_box(self, label_text, items, default_value):
+        label = QLabel(label_text)
+        combo_box = QComboBox()
+        combo_box.addItems(items)
+        combo_box.setCurrentText(default_value)
+        return label, combo_box
 
     def create_layout(self):
         layout = QVBoxLayout()
@@ -147,64 +120,64 @@ class SpiConfigurations(QDialog):
         self.enable_combo.currentIndexChanged.disconnect(self.update_comboboxes)
 
     def update_comboboxes(self):
-        sender_combo_box = self.sender()
-        self.selected_channel = sender_combo_box.currentText()
+        self.sender_combo_box = self.sender()
+        self.selected_channel = self.sender_combo_box.currentText()
 
-        current_texts = {combo_box: combo_box.currentText() for combo_box in
-                         [self.mosi_combo, self.miso_combo, self.clock_combo, self.enable_combo]}
+        self.current_texts = {combo_box: combo_box.currentText() for combo_box in
+                              [self.mosi_combo, self.miso_combo, self.clock_combo, self.enable_combo]}
 
-        pins_values = [self.mosi_combo, self.miso_combo, self.clock_combo, self.enable_combo]
+        self.pins_values = [self.mosi_combo, self.miso_combo, self.clock_combo, self.enable_combo]
 
-        for combo_box in pins_values:
+        for combo_box in self.pins_values:
             combo_box.currentIndexChanged.disconnect(self.update_comboboxes)
             current_items = [combo_box.itemText(index) for index in range(combo_box.count())]
             combo_box.clear()
             items_without_selected = [item for item in current_items if item != self.selected_channel]
             combo_box.addItems(items_without_selected)
 
-            if combo_box is sender_combo_box:
-                self.update_sender_combobox(combo_box, current_texts, items_without_selected, pins_values, sender_combo_box)
+            if combo_box is self.sender_combo_box:
+                self.update_sender_combobox(combo_box, items_without_selected)
             else:
-                self.update_other_combobox(combo_box, current_texts, items_without_selected)
-
-            combo_box.model().sort(0)
+                self.update_other_combobox(combo_box, items_without_selected)
             combo_box.currentIndexChanged.connect(self.update_comboboxes)
 
-    def update_sender_combobox(self, combo_box, current_texts, items_without_selected, pins_values, sender_combo_box):
+    def update_sender_combobox(self, combo_box, items_without_selected):
         if self.selected_channel != "Select Channel":
             if self.selected_channel not in items_without_selected:
                 combo_box.addItem(self.selected_channel)
 
         combo_box.setCurrentText(self.selected_channel)
-        combo_box_index = pins_values.index(combo_box) + 1
-        self.prev_value = getattr(self, f"prev_b{combo_box_index}")
+        self.combo_box_index = self.pins_values.index(combo_box) + 1
+        self.prev_value = getattr(self, f"prev_b{self.combo_box_index}")
 
-        self.update_prev_value(combo_box, items_without_selected, pins_values, sender_combo_box, current_texts, combo_box_index)
+        self.update_prev_value(combo_box, items_without_selected)
+        combo_box.model().sort(0)
 
-    def update_prev_value(self, combo_box, items_without_selected, pins_values, sender_combo_box, current_texts, combo_box_index):
+    def update_prev_value(self, combo_box, items_without_selected):
         if self.prev_value != "Select Channel" and self.prev_value != self.selected_channel:
             if self.prev_value not in items_without_selected:
                 combo_box.addItem(self.prev_value)
         combo_box.setCurrentText(self.selected_channel)
 
-        for other_combo_box in pins_values:
-            if other_combo_box is not sender_combo_box and self.selected_channel != "Select Channel":
+        for other_combo_box in self.pins_values:
+            if other_combo_box is not self.sender_combo_box and self.selected_channel != "Select Channel":
                 if self.prev_value not in [other_combo_box.itemText(index) for index in
                                            range(other_combo_box.count())]:
                     other_combo_box.addItem(self.prev_value)
-                other_combo_box.setCurrentText(current_texts[other_combo_box])
+                other_combo_box.setCurrentText(self.current_texts[other_combo_box])
 
-        setattr(self, f"prev_b{combo_box_index}", self.selected_channel)
+        setattr(self, f"prev_b{self.combo_box_index}", self.selected_channel)
 
-    def update_other_combobox(self, combo_box, current_texts, items_without_selected):
+    def update_other_combobox(self, combo_box, items_without_selected):
         if self.prev_value != self.selected_channel and self.prev_value != "Select Channel":
             if self.prev_value not in items_without_selected:
                 combo_box.addItem(self.prev_value)
-        if (current_texts[combo_box] != "Select Channel" and current_texts[combo_box] not in
+        if (self.current_texts[combo_box] != "Select Channel" and self.current_texts[combo_box] not in
                 items_without_selected):
-            if current_texts[combo_box] not in items_without_selected:
-                combo_box.addItem(current_texts[combo_box])
-        combo_box.setCurrentText(current_texts[combo_box])
+            if self.current_texts[combo_box] not in items_without_selected:
+                combo_box.addItem(self.current_texts[combo_box])
+        combo_box.setCurrentText(self.current_texts[combo_box])
+        combo_box.model().sort(0)
 
     def get_selected_channels(self):
         mosi = self.mosi_combo.currentText()
