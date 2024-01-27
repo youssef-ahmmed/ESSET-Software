7484c611b4fd63cb653ccbab94810431594b382d
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2024-01-27 20:52:05 +0200

    feat[backend]: add channel name column to database

--- models/dto/channels_data_dto.py
+++ models/dto/channels_data_dto.py
@@ -2,7 +2,8 @@ from typing import Any
 
 
 class ChannelsDataDto:
-    def __init__(self, sniffed_data_id: int, channel_number: int, channel_data: Any):
+    def __init__(self, sniffed_data_id: int, channel_number: int, channel_data: Any, channel_name: str):
         self.sniffed_data_id = sniffed_data_id
         self.channel_number = channel_number
         self.channel_data = channel_data
+        self.channel_name = channel_name
--- models/entities/channels_data.py
+++ models/entities/channels_data.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, BLOB
+from sqlalchemy import Column, Integer, ForeignKey, BLOB, String
 from sqlalchemy.orm import relationship
 
 from models.entities.base_model import Base, BaseModel
@@ -11,5 +11,6 @@ class ChannelsData(BaseModel, Base):
     sniffed_data_id = Column(Integer, ForeignKey('sniffed_data.id'))
     channel_number = Column(Integer, nullable=False)
     channel_data = Column(BLOB)
+    channel_name = Column(String(10), nullable=False)
 
     sniffed_data = relationship('SniffedData', backref='channels_data', cascade='all, delete')
