7015a84fd7646d14e854a9ce7c378efc2552f386
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2024-01-27 21:02:49 +0200

    refactor[controller]: change connection way names

--- controllers/sniffing_controller/bits_input_dialog_controller.py
+++ controllers/sniffing_controller/bits_input_dialog_controller.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QMessageBox
 
 from controllers.project_path_controller import ProjectPathController
+from controllers.sniffing_controller.number_bits_select_controller import NumberBitsSelectController
 from core.qsf_writer import QsfWriter
 from core.vhdl_generator import VhdlGenerator
 
@@ -42,41 +43,45 @@ class BitsInputDialogController(QObject):
 
         bits_number = self.get_bits_number()
         if bits_number is not None:
-            print(f"Number of bits: {bits_number}")
             self.bits_input_dialog.accept()
-            self.render_NBit_templates()
+            self.render_bit_templates()
 
     def get_bits_number(self):
         no_of_bits = self.bits_input_dialog.bits_input.text()
         if not no_of_bits:
             QMessageBox.warning(self.bits_input_dialog, "Warning", "Please enter the number of bits.")
             return None
-        self.bits_input_dialog.bits_input.clear()
-
-        self.NBit_configurations = {
-            'option': 'NBit_Sniffing',
+        self.bits_number = NumberBitsSelectController.get_instance().get_selected_option()
+        self.bit_configurations = {
+            'option': self.bits_number,
             'top_level_name': ProjectPathController.get_instance().get_top_level_name(),
-            'channels_number': no_of_bits
+            'channel_number': no_of_bits
         }
 
-        return self.NBit_configurations
+        return self.bit_configurations
 
-    def render_NBit_templates(self):
+    def render_bit_templates(self):
         vhdl_generator = VhdlGenerator()
         qsf_writer = QsfWriter()
+        bit_template = ''
+
+        if self.bits_number == "NBits":
+            bit_template = 'NBit_Sniffing.vhd.jinja'
+        elif self.bits_number == "1Bit":
+            bit_template = '1Bit_Sniffing.vhd.jinja'
 
         templates = [
             'top_level.vhd.jinja',
-            'NBit_Sniffing.vhd.jinja',
+            bit_template,
             'Common_Ports.vhd.jinja',
             'Communication_Module.vhd.jinja'
         ]
 
         for template in templates:
             vhdl_generator.render_template(template_name=template,
-                                           configurations=self.NBit_configurations, output_path=self.project_path)
+                                           configurations=self.bit_configurations, output_path=self.project_path)
 
         synthesis_template = 'synthesis_linux.sh.jinja' if platform.system() == 'Linux' else 'synthesis_windows.bat.jinja'
         vhdl_generator.render_template(template_name=synthesis_template,
-                                       configurations=self.NBit_configurations, output_path=self.project_path)
+                                       configurations=self.bit_configurations, output_path=self.project_path)
         qsf_writer.write_vhdl_files_to_qsf()
