a28c5b8bf1249fdbb99cbf48b65401f6eb0f5d88
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-01-28 18:21:24 +0200

    refactor[backend]: remove direction column from channel pins table

--- models/dto/channel_pins_dto.py
+++ models/dto/channel_pins_dto.py
@@ -1,6 +1,5 @@
 class ChannelPinsDto:
-    def __init__(self, sniffed_data_id: int, channel_name: str, direction: str, hardware_pin: str):
+    def __init__(self, sniffed_data_id: int, channel_name: str, hardware_pin: str):
         self.sniffed_data_id = sniffed_data_id
         self.channel_name = channel_name
-        self.direction = direction
         self.hardware_pin = hardware_pin
