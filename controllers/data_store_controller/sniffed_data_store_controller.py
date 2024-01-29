cb348e1364a444d8971eb35bdcfb43bb85f70fa1
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-29 17:06:20 +0200

    refactor[controller]: pass `time_taken` throw method argument

--- controllers/data_store_controller/sniffed_data_store_controller.py
+++ controllers/data_store_controller/sniffed_data_store_controller.py
@@ -1,7 +1,8 @@
-from controllers.data_collector_controller import DataCollectorController
-from models.dto.sniffed_data_dto import SniffedDataDto
-from models.dao.sniffed_data_dao import SniffedDataDao
 from datetime import datetime
+
+from controllers.data_collector_controller import DataCollectorController
+from models.dao.sniffed_data_dao import SniffedDataDao
+from models.dto.sniffed_data_dto import SniffedDataDto
 
 
 class SniffedDataStoreController:
@@ -11,8 +12,9 @@ class SniffedDataStoreController:
         self.sniffed_data_dto = None
         self.sniffed_data_dao = None
 
-    def store_sniffed_data(self):
-        self.sniffed_data_dto = SniffedDataDto(datetime.now(), 0,
-                                               **self.data_collector_controller.get_sniffed_data())
+    def store_sniffed_data(self, time_taken):
+        self.sniffed_data_dto = SniffedDataDto(datetime.now(),
+                                               time_taken,
+                                               **self.data_collector_controller.collect_sniffed_data())
         self.sniffed_data_dao = SniffedDataDao(self.sniffed_data_dto)
         self.sniffed_data_dao.insert()
