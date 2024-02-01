c68e483674351558b9182d83ec0b0b94bd84a935
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-02-01 18:59:56 +0200

    feat[frontend]: add custom widget for configuration buttons

--- views/sniffing/buttons/configuration_buttons.py
+++ views/sniffing/buttons/configuration_buttons.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QHBoxLayout
+from qfluentwidgets import FluentIcon as FIF
+from qfluentwidgets import PrimaryPushButton
 
 from controllers.sniffing_controller.buttons_controller.start_sniffing_button_controller import \
     StartSniffingButtonController
@@ -11,13 +13,13 @@ class ConfigurationButtons(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.synthesis_button = QPushButton('Synthesis')
-        self.start_sniffing = QPushButton('Start Sniffing')
+        self.synthesis_button = PrimaryPushButton(FIF.ROBOT, 'Synthesis')
+        self.start_sniffing = PrimaryPushButton(FIF.IOT, 'Start Sniffing')
         self.sniffing_timer_dialog = SniffingTimer(self.start_sniffing)
 
-        SynthesisButtonController.get_instance(self.synthesis_button)
-        StartSniffingButtonController.get_instance(self.start_sniffing)
-        SniffingTimerDialogController.get_instance(self.sniffing_timer_dialog)
+        SynthesisButtonController.get_instance(parent, self.synthesis_button)
+        StartSniffingButtonController.get_instance(parent, self.start_sniffing)
+        SniffingTimerDialogController.get_instance(parent, self.sniffing_timer_dialog)
 
         self.init_ui()
 
