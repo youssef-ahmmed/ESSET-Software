31d27769816bf6e3a5087d962cd16e92e380f415
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-02-01 18:50:09 +0200

    feat[frontend]: add custom widgets for dropdowns

--- views/sniffing/comm_protocol_select.py
+++ views/sniffing/comm_protocol_select.py
@@ -1,51 +1,41 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QStackedWidget, QComboBox, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout
+from qfluentwidgets import ComboBox
 
 from controllers.sniffing_controller.communication_protocol_controller.spi_dialog_controller import SpiDialogController
-from controllers.sniffing_controller.communication_protocol_controller.uart_dialog_controller import UartDialogController
+from controllers.sniffing_controller.communication_protocol_controller.uart_dialog_controller import \
+    UartDialogController
 from views.sniffing.communication_protocols.spi_config import SpiConfigurations
 from views.sniffing.communication_protocols.uart_config import UartConfigurations
 
 
 class CommunicationProtocolSelect(QWidget):
 
-    def __init__(self):
+    def __init__(self, parent=None):
         super().__init__()
 
-        self.spi_page = SpiConfigurations()
-        self.spi_controller = SpiDialogController.get_instance(self.spi_page)
+        self.setGeometry(100, 100, 400, 300)
 
-        self.uart_page = UartConfigurations()
-        self.uart_controller = UartDialogController.get_instance(self.uart_page)
+        self.spi_page = SpiConfigurations(parent)
+        self.spi_controller = SpiDialogController.get_instance(parent, self.spi_page)
 
+        self.uart_page = UartConfigurations(parent)
+        self.uart_controller = UartDialogController.get_instance(parent, self.uart_page)
+
+        self.selected_protocol = None
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Communication Settings")
-        self.setGeometry(100, 100, 400, 300)
-
-        self.create_layout()
-
-    def create_layout(self):
         layout = QHBoxLayout()
+        comm_label = QLabel("Communication Protocol")
 
-        self.comm_label = QLabel("Communication Protocol")
-        self.protocol_combo = QComboBox()
-        self.protocol_combo.addItem("Select Comm Protocol")
-        self.protocol_combo.addItems(["SPI", "UART", "I2C", "None"])
-        self.protocol_combo.setItemData(0, 0, role=Qt.UserRole - 1)
-        self.protocol_combo.setCurrentIndex(0)
-        self.protocol_combo.activated.connect(self.show_selected_settings)
+        self.protocol_combo = ComboBox()
+        self.protocol_combo.addItems(["Choose", "UART", "SPI", "I2C"])
 
-        self.protocol_pages = QStackedWidget()
-
-        self.protocol_pages.addWidget(self.spi_page)
-        self.protocol_pages.addWidget(self.uart_page)
-
-        layout.addWidget(self.comm_label)
+        layout.addWidget(comm_label)
         layout.addWidget(self.protocol_combo)
 
         self.setLayout(layout)
+        self.protocol_combo.currentIndexChanged.connect(self.show_selected_settings)
 
     def show_selected_settings(self, index):
         if index >= 0:
