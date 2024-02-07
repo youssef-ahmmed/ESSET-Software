abdd5e8d36d3b209110a03b3270be3b431fd7153
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-02-07 14:20:49 +0200

    feat[backend]: add hardware pins numbers

--- models/log_messages.py
+++ models/log_messages.py
@@ -32,3 +32,15 @@ N_BITS_CONFIG_SET = "N Bits Sniffing Configurations Set Successfully."
 
 # Hardware Pin Planner
 PINS_SET = "Hardware Pins Set Successfully."
+PINS_NUMBERS = [
+    'PIN_1', 'PIN_2', 'PIN_3 - ', 'PIN_7', 'PIN_10', 'PIN_11', 'PIN_23', 'PIN_25', 'PIN_28', 'PIN_30',
+    'PIN_31', 'PIN_32', 'PIN_33', 'PIN_34', 'PIN_38', 'PIN_39', 'PIN_42', 'PIN_43', 'PIN_44', 'PIN_46',
+    'PIN_49', 'PIN_50', 'PIN_51', 'PIN_52', 'PIN_53', 'PIN_54', 'PIN_55', 'PIN_58', 'PIN_59', 'PIN_60',
+    'PIN_64', 'PIN_65', 'PIN_66', 'PIN_67', 'PIN_68', 'PIN_69', 'PIN_70', 'PIN_71', 'PIN_72', 'PIN_73',
+    'PIN_74', 'PIN_75', 'PIN_76', 'PIN_77', 'PIN_80', 'PIN_83', 'PIN_84', 'PIN_85', 'PIN_86', 'PIN_87',
+    'PIN_88', 'PIN_89', 'PIN_90', 'PIN_91', 'PIN_98', 'PIN_99', 'PIN_100', 'PIN_101', 'PIN_103', 'PIN_104',
+    'PIN_105', 'PIN_106', 'PIN_110', 'PIN_112', 'PIN_113', 'PIN_114', 'PIN_115', 'PIN_119', 'PIN_120',
+    'PIN_121', 'PIN_124', 'PIN_125', 'PIN_126', 'PIN_127', 'PIN_128', 'PIN_129', 'PIN_132', 'PIN_133',
+    'PIN_135', 'PIN_136', 'PIN_137', 'PIN_138', 'PIN_141', 'PIN_142', 'PIN_143', 'PIN_144'
+]
+
