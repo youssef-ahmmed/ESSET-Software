899e46bec6e57feb481e61aeffed55d19d7f33ec
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-13 17:50:55 +0200

    feat[controller]: implement base class for getting display data

---
+++ controllers/display_controller/abstract_classes/data_display_collector_controller.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import QObject
+
+from controllers.display_controller.search_timestamp_controller import SearchTimestampController
+from core.data_processing import DataProcessing
+
+
+class DataDisplayCollectorController(QObject):
+
+    @staticmethod
+    def _get_channels_data(channels_data):
+        terminal_data = ""
+        for row in channels_data:
+            terminal_data += str(row.channel_data)[2:-1]
+        return terminal_data
+
+    @staticmethod
+    def _get_plot_data(channels_data):
+        plot_data: list[dict] = []
+        for row in channels_data:
+            plot_data.append({
+                'channel_name': row.channel_name,
+                'channel_number': row.channel_number,
+                'channel_data': DataProcessing(row.channel_data).convert_byte_list_to_bit_list()
+            })
+
+        return plot_data
