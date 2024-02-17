457710102c73eab5dcdc776c50b1c0fa12f921c5
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-02-17 02:10:59 +0200

    refactor[controller]: sniffing data collector controller

--- controllers/sniffing_controller/data_sniffing_collector_controller.py
+++ controllers/sniffing_controller/data_sniffing_collector_controller.py
@@ -20,76 +20,51 @@ class DataCollectorController(QObject):
     @staticmethod
     def collect_uart_data():
         uart_data = UartDialogController.get_instance().collect_uart_settings()
-        uart_data.pop('top_level_name', None)
-        uart_data.pop('option', None)
-        uart_data.pop('channel_number', None)
-        uart_data.pop('channel_name', None)
-        uart_data['data_size'] = int(uart_data['data_size'])
-        uart_data['stop_bit'] = int(uart_data['stop_bit'])
-        return uart_data
+        database_keys = ['clk_per_bit', 'baud_rate', 'data_size', 'stop_bit', 'parity_bit', 'significant_bit']
+        return {key: uart_data[key] for key in database_keys}
 
     @staticmethod
     def collect_spi_data():
         spi_data = SpiDialogController.get_instance().collect_spi_settings()
-        return {
-            'clock_rate': spi_data['clock_rate'],
-            'significant_bit': spi_data['significant_bit'],
-            'clk_state': int(spi_data['clk_state']),
-            'clk_phase': int(spi_data['clk_phase']),
-            'data_size': int(spi_data['data_size']),
-        }
+        database_keys = ['clock_rate', 'significant_bit', 'clk_state', 'clk_phase', 'data_size']
+        return {key: spi_data[key] for key in database_keys}
 
     @staticmethod
-    def collect_one_bit_data():
+    def collect_bits_data():
         bits_data = BitsInputDialogController.get_instance().get_bits_configurations()
-        option = bits_data.pop('option', None)
-        if option == "NBits":
-            return
-        bits_data.pop('top_level_name', None)
-
-        if option == "One_Bit":
-            bits_data["output_channel_number"] = bits_data.pop("channel_number")
-
-        return bits_data
-
-    @staticmethod
-    def collect_n_bit_data():
-        bits_data = BitsInputDialogController.get_instance().get_bits_configurations()
-        option = bits_data.pop('option', None)
-        if option == "One_Bit":
-            return
-        bits_data.pop('top_level_name', None)
-        if option == "NBits":
-            return bits_data
+        if bits_data['option'] == "One_Bit":
+            return {
+                'output_channel_number': bits_data['channel_number'],
+                'clock_rate': bits_data['clock_rate']
+            }
+        elif bits_data['option'] == "NBits":
+            return {
+                'channel_number': bits_data['channel_number'],
+                'clock_rate': bits_data['clock_rate']
+            }
+        return
 
     @staticmethod
     def collect_pin_planner_data():
         return PinPlannerDialogController.get_instance().get_pin_planner_data()
 
     def collect_channels_data(self):
-        communication_protocol = self.get_communication_protocol()
-        connection_way = self.get_connection_way()
-        if communication_protocol:
-            return communication_protocol
-        if connection_way:
-            return connection_way
+        return self.get_communication_protocol_channels() or self.get_connection_way_channels()
 
-    def collect_sniffed_data(self):
+    def collect_sniffing_option(self):
         comm_protocol = CommProtocolSelectController.get_instance().get_selected_option()
         connection_way = NumberBitsSelectController.get_instance().get_selected_option()
         if comm_protocol not in self.COMM_PROTOCOL:
             comm_protocol = None
         if connection_way not in self.CONNECTION_WAY:
             connection_way = None
-        sniffed_data = {
+        return {
             'connection_way': connection_way,
             'communication_protocol_name': comm_protocol
         }
-        return sniffed_data
 
-    def get_communication_protocol(self):
-        sniffed_data = self.collect_sniffed_data()
-        selected_protocol = sniffed_data['communication_protocol_name']
+    def get_communication_protocol_channels(self):
+        selected_protocol = self.collect_sniffing_option()['communication_protocol_name']
         if selected_protocol == 'Choose':
             return
         elif selected_protocol == "UART":
@@ -100,15 +75,14 @@ class DataCollectorController(QObject):
         elif selected_protocol == "SPI":
             spi_data = SpiDialogController.get_instance().collect_spi_settings()
             return {
-                'MOSI': int(spi_data['MOSI'][-1]),
-                'MISO': int(spi_data['MISO'][-1]),
-                'Clock': int(spi_data['Clock'][-1]),
-                'Enable': int(spi_data['Enable'][-1])
+                'MOSI': spi_data['MOSI'][-1],
+                'MISO': spi_data['MISO'][-1],
+                'Clock': spi_data['Clock'][-1],
+                'Enable': spi_data['Enable'][-1]
             }
 
-    def get_connection_way(self):
-        sniffed_data = self.collect_sniffed_data()
-        selected_connection_way = sniffed_data['connection_way']
+    def get_connection_way_channels(self):
+        selected_connection_way = self.collect_sniffing_option()['connection_way']
         if selected_connection_way == 'Choose':
             return
         elif selected_connection_way == '1Bit':
@@ -116,7 +90,7 @@ class DataCollectorController(QObject):
                 'Channel 1': self.DEFAULT_CHANNEL_NUMBER
             }
         elif selected_connection_way == 'NBits':
-            channel_number = self.collect_n_bit_data().get('channel_number') + 1
+            channel_number = self.collect_bits_data().get('channel_number') + 1
             return {
                 f'Channel {num}': num for num in range(1, channel_number)
             }
