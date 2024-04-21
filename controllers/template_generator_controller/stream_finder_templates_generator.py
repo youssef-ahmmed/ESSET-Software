20ca222fec1abb28dca164032a53849f64f5b077
sara-salah1 <67710906+sara-salah1@users.noreply.github.com>
2024-04-21 22:22:21 +0200

    refactor[controller]: add template mapping dictionary

--- controllers/template_generator_controller/stream_finder_templates_generator.py
+++ controllers/template_generator_controller/stream_finder_templates_generator.py
@@ -4,32 +4,26 @@ from controllers.template_generator_controller.template_generator_controller imp
 
 class StreamFinderTemplatesGenerator(TemplateGeneratorController):
 
+    TEMPLATE_MAPPING = {
+        "Flip Bits": {
+            "UART": ["StreamFinderWithFlipBits.vhd.jinja", "UART_Receiver.vhd.jinja", "UART_Transmitter.vhd.jinja"],
+            "SPI": ["StreamFinderWithFlipBits.vhd.jinja", "SPI_Slave.vhd.jinja", "SPI_Master.vhd.jinja"]
+        },
+        "Drop Communication": {
+            "UART": ["ConditionalByPass.vhd.jinja", "AndGate.vhd.jinja", "StreamBuffer.vhd.jinja",
+                     "UART_Receiver.vhd.jinja"],
+            "SPI": ["ConditionalByPass.vhd.jinja", "AndGate.vhd.jinja", "StreamBuffer.vhd.jinja", "SPI_Slave.vhd.jinja"]
+        },
+        "Raise Flag": {
+            "UART": ["StreamFinder.vhd.jinja", "UART_Receiver.vhd.jinja", "UART_Transmitter.vhd.jinja"],
+            "SPI": ["StreamFinder.vhd.jinja", "SPI_Slave.vhd.jinja", "SPI_Master.vhd.jinja"]
+        }
+    }
+
     def __init__(self):
         super().__init__()
         self.stream_finder_action = StreamFinderActionsController.get_instance().get_selected_stream_finder_action()
 
     def render_stream_finder_templates(self, configurations, option):
-        render_functions = {
-            "Flip Bits": self.render_stream_finder_with_flip_bits_templates,
-            "Drop Communication": self.render_conditional_bypass_templates,
-            "Raise Flag": self.render_stream_finder_with_raise_flags_templates,
-        }
-        render_functions.get(self.stream_finder_action)(configurations, option)
-
-    def render_stream_finder_with_raise_flags_templates(self, configurations, option="UART"):
-        template_names = self._get_template_names("StreamFinder.vhd.jinja", option)
-        self.render_templates(template_names, configurations)
-
-    def render_stream_finder_with_flip_bits_templates(self, configurations, option="UART"):
-        template_names = self._get_template_names("StreamFinderWithFlipBits.vhd.jinja", option)
-        self.render_templates(template_names, configurations)
-
-    def render_conditional_bypass_templates(self, configurations, option="UART"):
-        template_names = self._get_template_names("ConditionalByPass.vhd.jinja", option)
-        self.render_templates(template_names, configurations)
-
-    def _get_template_names(self, base_template, option):
-        if option == "UART":
-            return [base_template, 'UART_Receiver.vhd.jinja', 'UART_Transmitter.vhd.jinja']
-        else:
-            return [base_template, 'SPI_Slave.vhd.jinja', 'SPI_Master.vhd.jinja']
+        templates = self.TEMPLATE_MAPPING[self.stream_finder_action][option]
+        self.render_templates(templates, configurations)
