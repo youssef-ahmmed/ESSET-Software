79441f273da3cc4d6a1196dfe98428afdd46db77
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-22 05:49:27 +0200

    feat[frontend]: implement message response widget

---
+++ views/fuzzing/message_response_widget.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+from views.fuzzing.messages_progress_bar import MessageProgressBar
+from views.fuzzing.response_table import ResponseTable
+from views.fuzzing.response_table_buttons import ResponseTableButtons
+
+
+class MessageResponseWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.response_table = ResponseTable()
+        self.message_progress_bar = MessageProgressBar()
+        self.response_table_buttons = ResponseTableButtons()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.response_table)
+        self.layout().addWidget(self.message_progress_bar)
+        self.layout().addWidget(self.response_table_buttons)
