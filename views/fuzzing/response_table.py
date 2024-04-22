00333bb5cc744c4edc8988ec2cbe51dd17a68a4e
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-22 21:15:18 +0200

    feat[frontend]: add column for message details

--- views/fuzzing/response_table.py
+++ views/fuzzing/response_table.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableWidgetItem
-from qfluentwidgets import TableWidget
+from qfluentwidgets import FluentIcon as FIF
+from qfluentwidgets import TableWidget, ToolButton
 
 
 class ResponseTable(QWidget):
@@ -10,9 +11,9 @@ class ResponseTable(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.response_table.setColumnCount(2)
-        self.response_table.setRowCount(1000)
-        self.response_table.setHorizontalHeaderLabels(["Message", "Response"])
+        self.response_table.setColumnCount(3)
+        self.response_table.setRowCount(100)
+        self.response_table.setHorizontalHeaderLabels(["Message", "Response", "Info"])
 
         layout = QVBoxLayout()
         layout.addWidget(self.response_table)
@@ -25,11 +26,14 @@ class ResponseTable(QWidget):
             item2 = QTableWidgetItem(f"Response {row + 1}")
             self.response_table.setItem(row, 0, item1)
             self.response_table.setItem(row, 1, item2)
+            self.response_table.setCellWidget(row, 2, ToolButton(FIF.MESSAGE, self.response_table))
 
     def table_resize_event(self, event):
         available_width = event.size().width()
-        column0_width = int(available_width * 0.5)
-        column1_width = int(available_width * 0.5)
+        column0_width = int(available_width * 0.4)
+        column1_width = int(available_width * 0.4)
+        column2_width = int(available_width * 0.1)
 
         self.response_table.setColumnWidth(0, column0_width)
         self.response_table.setColumnWidth(1, column1_width)
+        self.response_table.setColumnWidth(2, column2_width)
