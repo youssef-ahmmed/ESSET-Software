26dcd4431d3902dc275642fc23cf2e788db22921
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-04-25 21:45:16 +0200

    feat[view]: update response table to create dynamic rows

--- views/fuzzing/response_table.py
+++ views/fuzzing/response_table.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableWidgetItem
 from qfluentwidgets import FluentIcon as FIF
 from qfluentwidgets import TableWidget, ToolButton
@@ -12,7 +13,7 @@ class ResponseTable(QWidget):
 
     def init_ui(self):
         self.response_table.setColumnCount(3)
-        self.response_table.setRowCount(100)
+
         self.response_table.setHorizontalHeaderLabels(["Message", "Response", "Info"])
 
         layout = QVBoxLayout()
@@ -20,13 +21,26 @@ class ResponseTable(QWidget):
         self.setLayout(layout)
         self.response_table.resizeEvent = self.table_resize_event
 
-        #TODO: to be deleted
-        for row in range(100):
-            item1 = QTableWidgetItem(f"Message {row + 1}")
-            item2 = QTableWidgetItem(f"Response {row + 1}")
-            self.response_table.setItem(row, 0, item1)
-            self.response_table.setItem(row, 1, item2)
+    def create_table_rows(self, random_data: list):
+        number_of_rows = len(random_data)
+        self.response_table.setRowCount(number_of_rows)
+        for row in range(number_of_rows):
+            if isinstance(random_data[row], list):
+                message = QTableWidgetItem(''.join(map(str, random_data[row])))
+            else:
+                message = QTableWidgetItem(random_data[row])
+            response = QTableWidgetItem("Response")
+
+            message.setFlags(message.flags() & ~Qt.ItemIsEditable)
+            response.setFlags(response.flags() & ~Qt.ItemIsEditable)
+
+            self.response_table.setItem(row, 0, message)
+            self.response_table.setItem(row, 1, response)
             self.response_table.setCellWidget(row, 2, ToolButton(FIF.MESSAGE, self.response_table))
+
+    def delete_all_data(self):
+        self.response_table.clearContents()
+        self.response_table.setRowCount(0)
 
     def table_resize_event(self, event):
         available_width = event.size().width()
