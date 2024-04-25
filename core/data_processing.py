df663526dc9b35468a63b7ff15ceb1112e0c5ded
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-04-25 21:41:18 +0200

    feat[core]: add methods to concat random generated data to string and hex

--- core/data_processing.py
+++ core/data_processing.py
@@ -17,3 +17,23 @@ class DataProcessing:
     def convert_decimal_to_hex(self):
         decimal_data = self.convert_data_to_decimal()
         return [f'/x{hex(byte)[2:].zfill(2).upper()}' for byte in decimal_data]
+
+    def combine_data_to_string(self):
+        combined_data = []
+        for item in self.data:
+            if isinstance(item, list):
+                combined_data.append(''.join(map(str, item)))
+            else:
+                combined_data.append(item)
+        return ''.join(combined_data)
+
+    def combine_data_to_hex(self):
+        combined_hex = ""
+        for item in self.data:
+            if isinstance(item, list):
+                item_hex = ''.join([f"0x{byte:02X}" for byte in item])
+                combined_hex += '\\' + item_hex
+            else:
+                item_hex = ''.join([f"0x{byte:02X}" for byte in item.encode()])
+                combined_hex += '\\' + item_hex
+        return combined_hex
