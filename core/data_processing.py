07a8005878e48d712defc4fda5cd4eee61074f7b
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-05-08 19:33:02 +0300

    feat[core]: add methods to combine fuzzed data as hex list

--- core/data_processing.py
+++ core/data_processing.py
@@ -2,6 +2,8 @@
 class DataProcessing:
     def __init__(self, data):
         self.data = data
+        self.fuzzed_string_data = None
+        self.fuzzed_hex_data = None
 
     def convert_data_to_decimal(self):
         return [int(byte) for byte in self.data]
@@ -18,22 +20,41 @@ class DataProcessing:
         decimal_data = self.convert_data_to_decimal()
         return [f'/x{hex(byte)[2:].zfill(2).upper()}' for byte in decimal_data]
 
-    def combine_data_to_string(self):
+    def combine_fuzzed_data_to_string(self):
+        if self.fuzzed_string_data:
+            return self.fuzzed_string_data
+
         combined_data = []
         for item in self.data:
             if isinstance(item, list):
                 combined_data.append(''.join(map(str, item)))
             else:
                 combined_data.append(item)
-        return ''.join(combined_data)
+        
+        self.fuzzed_string_data = ''.join(combined_data)
+        return self.fuzzed_string_data
 
-    def combine_data_to_hex(self):
-        combined_hex = ""
+    def combine_fuzzed_data_to_hex_string(self):
+        if self.fuzzed_hex_data:
+            return self.fuzzed_hex_data
+
+        self.fuzzed_hex_data = ""
         for item in self.data:
             if isinstance(item, list):
                 item_hex = ''.join([f"0x{byte:02X}" for byte in item])
-                combined_hex += '\\' + item_hex
+                self.fuzzed_hex_data += '\\' + item_hex
             else:
                 item_hex = ''.join([f"0x{byte:02X}" for byte in item.encode()])
-                combined_hex += '\\' + item_hex
-        return combined_hex
+                self.fuzzed_hex_data += '\\' + item_hex
+
+        return self.fuzzed_hex_data
+
+    def combine_fuzzed_data_to_hex_list(self):
+        hex_list = []
+        for item in self.data:
+            if isinstance(item, list):
+                hex_list.append(''.join([f"0x{byte:02X}" for byte in item]))
+            else:
+                hex_list.append(''.join([f"0x{byte:02X}" for byte in item.encode()]))
+
+        return hex_list
