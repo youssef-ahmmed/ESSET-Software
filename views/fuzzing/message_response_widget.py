fd90358d2ec3ca99ff879134ddeb52916bd71c9e
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-05-08 19:25:20 +0300

    refactor[view]: remove progress bar widget

--- views/fuzzing/message_response_widget.py
+++ views/fuzzing/message_response_widget.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
 from controllers.fuzzing_controller.response_table_controller import ResponseTableController
-from views.fuzzing.messages_progress_bar import MessageProgressBar
 from views.fuzzing.response_table import ResponseTable
 from views.fuzzing.response_table_buttons import ResponseTableButtons
 
@@ -11,7 +10,6 @@ class MessageResponseWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.response_table = ResponseTable()
-        self.message_progress_bar = MessageProgressBar()
         self.response_table_buttons = ResponseTableButtons()
 
         ResponseTableController.get_instance(self.response_table)
@@ -20,5 +18,4 @@ class MessageResponseWidget(QWidget):
     def init_ui(self):
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.response_table)
-        self.layout().addWidget(self.message_progress_bar)
         self.layout().addWidget(self.response_table_buttons)
