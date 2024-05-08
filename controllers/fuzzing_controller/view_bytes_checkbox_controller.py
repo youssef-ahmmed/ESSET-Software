239ae975e6734d6b8625b86b82b480ecfd49499c
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-05-08 19:35:40 +0300

    refactor[controller]: rename methods of combining fuzzed data

--- controllers/fuzzing_controller/view_bytes_checkbox_controller.py
+++ controllers/fuzzing_controller/view_bytes_checkbox_controller.py
@@ -4,6 +4,7 @@ from qfluentwidgets import CheckBox
 from controllers.fuzzing_controller.fuzzing_terminal_controller import FuzzingTerminalController
 from controllers.fuzzing_controller.generate_button_controller import GenerateButtonController
 from core.data_processing import DataProcessing
+from core.generator_based_fuzzing import GeneratorBasedFuzzing
 
 
 class ViewBytesCheckboxController(QObject):
@@ -26,11 +27,14 @@ class ViewBytesCheckboxController(QObject):
         self.view_bytes_checkbox.stateChanged.connect(self.toggle_data_view)
 
     def toggle_data_view(self, state):
-        fuzzed_data = GenerateButtonController.get_instance().get_generated_fuzzed_data()
+        fuzzed_data = GeneratorBasedFuzzing.get_fuzzed_data()
+        if not fuzzed_data:
+            return
+
         data_processing = DataProcessing(fuzzed_data)
         if state:
-            data_as_hex = data_processing.combine_data_to_hex()
+            data_as_hex = data_processing.combine_fuzzed_data_to_hex_string()
             FuzzingTerminalController.get_instance().write_text(data_as_hex)
         else:
-            data_as_string = data_processing.combine_data_to_string()
+            data_as_string = data_processing.combine_fuzzed_data_to_string()
             FuzzingTerminalController.get_instance().write_text(data_as_string)
