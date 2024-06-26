bc22f07d23746d4515fc9ff6fcd6339a8ce89af8
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-06-26 16:38:23 +0300

    refactor[view]: remove simple mode

--- views/sniffing/sniffing_modes_widget.py
+++ views/sniffing/sniffing_modes_widget.py
@@ -1,9 +1,8 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QTabWidget, QHBoxLayout, QSplitter
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSplitter
 
-from views.sniffing.expert_mode_widget import ExpertModeWidget
 from views.sniffing.hardware_configuration_widget import HardwareConfigurations
-from views.sniffing.simple_mode_widget import SimpleModeWidget
+from views.sniffing.vhdl_widget import VhdlWidget
 
 
 class SniffingModesWidget(QWidget):
@@ -11,32 +10,23 @@ class SniffingModesWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.modes_widget = QTabWidget(self)
+        self.vhdl_widget = VhdlWidget()
         self.hardware_config = HardwareConfigurations()
 
         self.init_ui()
 
     def init_ui(self):
         self.setLayout(QHBoxLayout())
-
-        self.layout().addWidget(self.modes_widget)
+        self.layout().addWidget(self.vhdl_widget)
         self.layout().addWidget(self.hardware_config)
 
-        simple_mode = SimpleModeWidget()
-        expert_mode = ExpertModeWidget()
-
-        self.modes_widget.addTab(expert_mode, "Expert Mode")
-        self.modes_widget.addTab(simple_mode, "Simple Mode")
-
         splitter = QSplitter(Qt.Horizontal)
-        splitter.addWidget(self.modes_widget)
+        splitter.addWidget(self.vhdl_widget)
         splitter.addWidget(self.hardware_config)
 
-        total_size = self.modes_widget.sizeHint().width() + self.hardware_config.sizeHint().width()
-
+        total_size = self.vhdl_widget.sizeHint().width() + self.hardware_config.sizeHint().width()
         size_vhdl = 0.99 * total_size
         size_settings = 0.01 * total_size
 
         splitter.setSizes([int(size_vhdl), int(size_settings)])
-
         self.layout().addWidget(splitter)
